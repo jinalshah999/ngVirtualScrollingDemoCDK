3107bf9c4e4d47004d5d887151352eb206e8845e
jinalshah999 <jinalshah999@gmail.com>
2018-10-23 15:17:40 +0530

    added virtual scrolling demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import * as faker from 'faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'ngScrollingDemoCDK';
+students;
+  constructor()
+  {
+      this.students=Array(100).fill(1).map(_=>{
+        return{
+          name:faker.name.findName(),
+          email:faker.internet.email()
+        };
+      });
+
+  }
 }
---
+++ src/app/student.ts
@@ -0,0 +1,5 @@
+export class student{
+  public constructor(public name:string,public bio:string){
+
+  }
+}
